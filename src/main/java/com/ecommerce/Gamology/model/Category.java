784e669bbff903ceba6742cb7efb5bb99faca9ac
Mohammed Haaroon <mohammedhaaroon@anugraha.co>
2026-01-30 18:06:27 +0530

    Switched to lombok and custom exception handling

--- src/main/java/com/ecommerce/Gamology/model/Category.java
+++ src/main/java/com/ecommerce/Gamology/model/Category.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "Categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
@@ -14,30 +20,4 @@ public class Category {
     private String categoryName;
 
 
-    public Category(String categoryName, Long categoryId) {
-        this.categoryName = categoryName;
-        this.categoryId = categoryId;
-
-    }
-
-    public Category() {
-
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
 }
